a5f43345da09c8460c01d5711ecb760e168bb235
Gabriel A. Orellana <gabi.ale.orellana@gmail.com>
2026-07-27 21:23:46 -0300

    Primer Commit: inicio de repositorio.

---
+++ src/main/java/com/barberia/GestionBarberia/GestionBarberiaApplication.java
@@ -0,0 +1,13 @@
+package com.barberia.GestionBarberia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionBarberiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionBarberiaApplication.class, args);
+	}
+
+}
