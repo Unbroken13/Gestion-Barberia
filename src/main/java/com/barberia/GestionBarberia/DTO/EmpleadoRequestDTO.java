f022cb946ee1a4d2591a34479c421b983e2c2de1
Gabriel A. Orellana <gabi.ale.orellana@gmail.com>
2026-08-27 22:14:40 -0300

    feat(empleado): implementar DTOs, mappers y servicios para Empleado

--- src/main/java/com/barberia/GestionBarberia/DTO/EmpleadoRequestDTO.java
+++ src/main/java/com/barberia/GestionBarberia/DTO/EmpleadoRequestDTO.java
@@ -1,18 +1,20 @@
 package com.barberia.GestionBarberia.DTO;
 
 
+import com.barberia.GestionBarberia.model.Rol;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
 
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class CustomerRequestDTO {
+public class EmpleadoRequestDTO {
 
     private String nombre;
     private String apellido;
@@ -21,5 +23,13 @@ public class CustomerRequestDTO {
     private String telefono;
     private String email;
     private LocalDate fechaNacimiento;
+
+    //atributos específicos
+    private Rol rol;
+    private String especialidad;
+    private LocalDate fechaContratacion;
+    private BigDecimal porcentajeComision;
+    private Boolean activo;
+
 
 }
--- src/main/java/com/barberia/GestionBarberia/DTO/EmpleadoResponseDTO.java
+++ src/main/java/com/barberia/GestionBarberia/DTO/EmpleadoResponseDTO.java
@@ -1,25 +1,36 @@
 package com.barberia.GestionBarberia.DTO;
 
-
+import com.barberia.GestionBarberia.model.Rol;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
 
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class CustomerRequestDTO {
+public class EmpleadoResponseDTO {
 
+    private Long id;
     private String nombre;
     private String apellido;
     private String username;
-    private String password;
     private String telefono;
     private String email;
     private LocalDate fechaNacimiento;
+
+    //atributos específicos
+
+    private Rol rol;
+    private String especialidad;
+    private LocalDate fechaContratacion;
+    private BigDecimal porcentajeComision;
+    private Boolean activo;
+
+
 
 }
