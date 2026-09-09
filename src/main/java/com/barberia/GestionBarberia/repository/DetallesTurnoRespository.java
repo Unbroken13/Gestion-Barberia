b12d00038a3665dbd01e4b52e0567e64724eb13d
Gabriel A. Orellana <gabi.ale.orellana@gmail.com>
2026-09-08 21:21:36 -0300

    feat(turno): crear TurnoRepository, DetalleTurnoRepository

---
+++ src/main/java/com/barberia/GestionBarberia/repository/DetallesTurnoRespository.java
@@ -0,0 +1,6 @@
+package com.barberia.GestionBarberia.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DetallesTurnoRespository extends JpaRepository<DetallesTurnoRespository, Long> {
+}
---
+++ src/main/java/com/barberia/GestionBarberia/repository/TurnoRepository.java
@@ -0,0 +1,9 @@
+package com.barberia.GestionBarberia.repository;
+
+import com.barberia.GestionBarberia.model.Turno;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TurnoRepository extends JpaRepository<Turno, Long> {
+
+
+}
